93232add2b742cad20e6f4a0b7382e7728e662fd
bossiernesto <bossi.ernestog@gmail.com>
2015-09-01 03:11:20 -0300

    Add example with https

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
-//==========
-//===EH LEE LA DOCUMENTACIÓN ACA=====
-//==http://expressjs.com/guide/routing.html===
-//==Para más detalles de la API MIRA ACA===
-//==http://expressjs.com/4x/api.html
+var fs = require('fs');
 var express = require('express');
-var app = express();
+var https = require('https');
+var bodyParser = require('body-parser');
+var key = fs.readFileSync('./key.pem');
+var cert = fs.readFileSync('./cert.pem')
+var https_options = {
+    key: key,
+    cert: cert
+};
+var PORT = 8000;
+var HOST = 'localhost';
+app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.configure(function(){
+    app.use(app.router);
 });
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
 
-  console.log('Example app listening at http://%s:%s', host, port);
+app.get('/pepe', function(req, res) {
+  res.send('Pepe');	
 });
+
+app.get('/algo', function(req, res) {
+  res.send('Bleh');	
+});
+
+app.post('/echo', function (req, res) {
+  res.json(req.body);
+});
+
+app.all('/echo/param/:id', function (req, res) {
+  res.json({
+    param: req.params.id
+  });
+});
+
+app.post('/echo/headers', function (req, res) {
+  res.json(req.headers);
+});
+
+app.get('/echo/qs', function (req, res) {
+  res.json(req.query);
+});
+
+server = https.createServer(https_options, app).listen(PORT, HOST);
+console.log('HTTPS Server listening on %s:%s', HOST, PORT);
+
---
+++ client.js
@@ -0,0 +1,38 @@
+var PORT = 8000;
+var HOST = 'localhost';
+var tls = require('tls');
+var fs = require('fs');
+
+var options = { ca: [ fs.readFileSync('./cert.pem') ] };
+var client = tls.connect(PORT, HOST, options, function() {
+    if (client.authorized) {
+        
+        console.log('Connected and authorized\n');
+        
+        client.on('close', function() {
+            console.log('Closing Socket...\n');
+            process.exit();
+        });
+        
+        process.stdin.pipe(client);
+        process.stdin.resume();
+        
+        // Time to make some request to the server
+        // We will write straight to the socket, but recommended way is to use a client library like 'request' or 'superagent'
+        client.write('GET /Pepe HTTP/1.1\r\n');
+        client.write('\r\n');
+        
+        client.write('POST /bleh HTTP/1.1\r\n');
+        client.write('\r\n');
+
+    }
+    else {
+        console.log('HTTPS Authentication failed\n');
+        process.exit();
+    }
+});
+client.setEncoding('utf8');
+client.on('data', function(data) {
+    console.log('-------------');
+    console.log(data);
+});
